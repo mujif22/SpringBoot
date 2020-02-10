0ca6bf1919a434e9c899b35c3a09856ae5e2a09b
mujif <mujif@Mujif>
2020-02-10 12:03:53 +0530

    Change Information

--- src/in/nit/Test.java
+++ src/in/nit/Test.java
@@ -14,21 +14,11 @@ public class Test {
 		return sid;
 	}
 
-	public void setSid(int sid) {
-		this.sid = sid;
-	}
-
-	public String getSname() {
-		return sname;
-	}
-
-	public void setSname(String sname) {
-		this.sname = sname;
-	}
-
 	@Override
 	public String toString() {
 		return "Test [sid=" + sid + ", sname=" + sname + ", address=" + address + "]";
 	}
+
+	
 
 }
